5eb1ab06a701ae4aca33ac5c4042fc9ad6e3b194
ZainUlAbedinZ <108004488+ZainUlAbedinZ@users.noreply.github.com>
2022-10-10 01:54:15 -0700

    Top Container

--- src/components/Traprenovatie/heroSectionTopContainer.js
+++ src/components/Traprenovatie/heroSectionTopContainer.js
@@ -12,43 +12,46 @@ const heroSectionTopContainer = props => {
 	return (
 		<>
         <Container>
-				<Row className='align-items-end' >
-					<Col lg='6' md='6' sm='12' xs='12' className='hero_section_col_1 align-items-end order-2 order-sm-2 order-md-1 order-xs-2'>
+				<Row className='align-items-end justify-content-end' >
+					<Col  md='5' xs='12' className='hero_section_col_1 px-3 align-items-end order-2 justify-content-end order-sm-2 order-md-1 order-xs-2 mr-5'>
 						{/* <div className='text-right pt-39 hero_section_col_1_text'> */}
-						<Row className='mt-5 mt-sm-2'>
-							<Col md='3' className='d-md-3'>
-								{/* d-sm-none d-xs-none */}
-							</Col>
-							<Col md='9' className='hero_section_col_1_text'>
+						{/* <Row className='mt-5 mt-sm-2 justify-content-end'> */}
+							{/* <Col md='3' className='d-md-3'>
+								
+							</Col> */}
+							{/* <Col md='9' className='hero_section_col_1_text justify-content-end'> */}
 							{/* <div className='d-block'>  */}
-							 <Row className='mt-1'>
-						<Col md='12' lg='12' sm='12' className='p-0'>
+							 {/* <Row className='mt-1'> */}
+						{/* <Col md='12' lg='12' sm='12' className='p-0'> */}
 							<div>
 						<img src={heroTextLogo} className='pl-3 pl-md-0 float-right' rounded width='130px' />
 						</div>
-						<h4 className='pl-4 pl-md-0 text-light text-left fw-800'>
+						<div className='p-0 fs-5'>
+						<h4 className='pl-0 fs-3 pl-md-0 text-light text-left fw-600'>
 							Gratis en Vrijblijvend <br />
 						 <span className='w-100'>offertes aanvragen bij een <br/> </span> 
 							netwerk van Speicallisten ?
 						</h4>
-							</Col>
+						
+							{/* </Col> */}
 							{/* <p className='text-light text-justify mt-1 mb-0 font-weight-normal w-100'> */}
-							<p className='text-light'></p>
+							{/* <p className='text-light'></p> */}
                                {/* </p > */}
-                            <p className='pl-3 pl-md-0 pl-md-0 pr-5 pr-md-3 text-justify text-light font-weight-normal shadow-sm'>
-                            <span className='w-100 font-weight-bold'> Zo krijg je de beste prijs voor jouw traprenovatie! 
+                            <p className='pl-3 pl-md-0 pl-md-0 fs-5 pr-5 pr-md-3 text-justify text-light fw-100 shadow-sm'>
+                            <span className='w-75 font-weight-bold'> Zo krijg je de beste prijs voor jouw traprenovatie! 
                             </span>
                                 .Is jouw trap ook toe aan een opknapbeaurt? Dan is dit jouw kans.Het
 							Kost je niets en je zit nergens aan vast. Zo kies jij zelf de bestie aanbieder.
 							Moeiteloos en zonder verplichtingen. Laat snel je gegevens achter en pak deze kans
 							</p>
-							</Row>
-							</Col>
-						</Row>
+							</div>
+							{/* </Row> */}
+							{/* </Col> */}
+						{/* </Row> */}
 					</Col>
-					<Col lg='6' md='6' sm='12' xs='12' className='hero_section_col_2 align-items-end order-sm-1 order-1 order-md-2 order-xs-1'>
-						<Row className='ml-2'>
-						<Col lg='9' md='9' sm='9'>
+					<Col  md='5' xs='12' className='hero_section_col_2 align-items-end order-sm-1 order-1 order-md-2 order-xs-1 pl-0 ml-0'>
+						{/* <Row className='ml-2'> */}
+						{/* <Col lg='9' md='9' sm='9'> */}
 						<img src={HeroTextTopLogo} className='mt-3 text-center' width='140px' />
 						<br />
 						{/* Text Top Header */}
@@ -58,8 +61,8 @@ const heroSectionTopContainer = props => {
 						<br />
 						<span className='font-weight-bold fs-1 text-site-text'>Vraag nu voor gratis offertes  aan voor jouw</span><span className='text-site-orange fs-1'> TRAPRENOVATIE </span> 
 						</p>
-						</Col>
-						</Row>
+						{/* </Col> */}
+						{/* </Row> */}
 						<Card
                        className="px-3 mt-3 mb-3 bg-site-primary w-100 w-sm-100 w-md-75 w-lg-75 w-xl-75  hero_section_col_2_Card"
                     >
