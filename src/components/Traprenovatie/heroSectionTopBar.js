bf82feb50032bbf5806a305776b35eeb50a4c050
ZainUlAbedinZ <108004488+ZainUlAbedinZ@users.noreply.github.com>
2022-10-11 14:42:39 +0500

    Topbar

--- src/components/Traprenovatie/heroSectionTopBar.js
+++ src/components/Traprenovatie/heroSectionTopBar.js
@@ -10,28 +10,46 @@ const MainNavbar = props => {
 	return (
 		<>
   <Container>
-{/* justify-content-center */}
-  <Row className='justify-content-center w-100 flex-wrap'>
+{/*  */}
+  <Row className=' w-100 flex-wrap flex-stretch'>
     {/* md='9 */}
-        <Col md='12' sm='12' className='d-md-flex d-col'>
+        <Col md='11' sm='12' className='d-md-flex d-col'>
+            
     <Col lg='3' md='3' sm='8' className='py-2  py-md-4  d-flex'>
-        <img src={Checkbox} color='light' className='text-light h-50 w-25' /> 
-           <strong className='d-inline-block text-left ml-2'>GRATIS en VERGELIJKN vergelijken </strong> 
+       <div >
+         {/*className='w-50'  */}
+       {/* h-50 w-25 */}
+        <img src={Checkbox} color='light' className='text-light' /> 
+       </div>
+           <div><strong className='d-inline-block text-left'>GRATIS en VERGELIJKN vergelijken </strong> 
+           </div>
           </Col>
       <Col lg='3' md='3' sm='8' className='py-2  py-md-4 d-flex '>
-      <img src={Checkbox} color='light'  className='text-light h-50 w-25' /> 
-     <strong className='d-inline-block text-left ml-2'>BESPAAR 30% in<span className='fs-6'>slechts 2 minuten</span></strong>
+        {/* h-50 w-25 */
+        }
+        {/* className='w-50' */}
+        <div >
+      <img src={Checkbox} color='light'  className='text-light ' /> 
+      </div>
+     <strong className='d-inline-block text-left'>BESPAAR 30% in<span className='fs-6'>slechts 2 minuten</span></strong>
       </Col>
-      <Col lg='3' md='3' sm='8' className='py-2 py-md-4  d-flex '>
-      <img src={Checkbox} color='light' className='text-light h-50 w-25' />
-       <strong className='d-inline-block text-left ml-2'>TRAPRENOVATIE al vanaf $ 2.500 </strong>
+      <Col lg='3' md='3' sm='8' className='py-2  py-md-4  d-flex '>
+        <div >
+            {/* className='w-50' */}
+            {/*  h-50 w-25 */}
+      <img src={Checkbox} color='light' className='text-light' />
+      </div>
+       <strong className='d-inline-block text-left'>TRAPRENOVATIE al vanaf $ 2.500 </strong>
           </Col>
-      <Col lg='3' md='3' sm='8' className='py-2 py-md-4  d-flex'>
-       <img src={Checkbox} color='light' className='text-light h-50 w-25 text-left' /> 
-       <strong className='d-inline-block text-left ml-2'>Binnen 1WERKDAG geplaatst</strong>
+      <Col lg='3' md='3' sm='8' className='py-2  py-md-4  d-flex'>
+      <div >
+        {/* className='w-50' */}
+        {/*  h-50 w-25 */}
+       <img src={Checkbox} color='light' className='text-light text-left' /> 
+       </div>
+       <strong className='d-inline-block text-left'>Binnen 1WERKDAG geplaatst</strong>
       </Col>
-      </Col>
-      
+      </Col>      
   </Row>
   
   </Container>
